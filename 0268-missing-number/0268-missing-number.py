dbf15ec9b6969a97a7b1ea856dbd3f41f66243bd
Zephyrus <130337647+Samiran2004@users.noreply.github.com>
2026-05-24 06:53:36 +0530

    Time: 0 ms (100%), Space: 20.5 MB (41.66%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        totalSum = n * (n + 1) // 2
+
+        arraySum = sum(nums)
+
+        missingNumber = totalSum - arraySum
+
+        return missingNumber
