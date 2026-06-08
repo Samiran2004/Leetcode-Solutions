490f0531ee17cfae6730b2e5b0404a57ed2fb3f7
Zephyrus <130337647+Samiran2004@users.noreply.github.com>
2026-06-08 07:20:14 +0530

    Time: 66 ms (15.2%), Space: 34 MB (17.69%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,20 +1,47 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        ans = []
+        # ans = []
 
-        # For less than pivot elements...
+        # # For less than pivot elements...
+        # for num in nums:
+        #     if num < pivot:
+        #         ans.append(num)
+        
+        # # For equal elements...
+        # for num in nums:
+        #     if num == pivot:
+        #         ans.append(num)
+        
+        # # For greater than pivot elements...
+        # for num in nums:
+        #     if num > pivot:
+        #         ans.append(num)
+        
+        # return ans
+
+
+        # Another Approach...
+        lCount = 0
+        pCount = 0
         for num in nums:
             if num < pivot:
-                ans.append(num)
+                lCount += 1
+            elif num == pivot:
+                pCount += 1
         
-        # For equal elements...
+        i = 0
+        j = lCount
+        k = lCount + pCount
+        res = [0] * len(nums)
+
         for num in nums:
-            if num == pivot:
-                ans.append(num)
-        
-        # For greater than pivot elements...
-        for num in nums:
-            if num > pivot:
-                ans.append(num)
-        
-        return ans
+            if num < pivot:
+                res[i] = num
+                i += 1
+            elif num == pivot:
+                res[j] = num
+                j += 1
+            elif num > pivot:
+                res[k] = num
+                k += 1
+        return res
