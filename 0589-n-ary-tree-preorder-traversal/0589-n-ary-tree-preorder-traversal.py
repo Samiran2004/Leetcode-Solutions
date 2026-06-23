712f8700ec8c56fa12c4f86409f25aeb81367dcb
Zephyrus <130337647+Samiran2004@users.noreply.github.com>
2026-06-23 08:33:05 +0530

    Time: 57 ms (42.19%), Space: 21 MB (21.1%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        def dfs(root, output):
+            if not root:
+                return None
+            
+            output.append(root.val)
+
+            for child in root.children:
+                dfs(child, output)
+            
+            return output
+        
+        return dfs(root, [])
